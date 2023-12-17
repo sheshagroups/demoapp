e043e948aaabc44d2dc87360478b9f301c5334d9
Romiyo43 <s.a.tech38@gmail.com>
2023-12-17 13:52:25 +0530

    Start Project.1.04

--- app/src/main/java/com/sheshagroups/satech/demoapp/C1A_Home.java
+++ app/src/main/java/com/sheshagroups/satech/demoapp/C1A_Home.java
@@ -1,6 +1,7 @@
 package com.sheshagroups.satech.demoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
@@ -15,6 +16,8 @@ public class C1A_Home extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ImageView menu;
     LinearLayout home,classwork,homework,dpp,fees,diary,notification,studentdetail,aboutschool,schoolalbum,contactus,logout;
+    CardView addstudent,uploadcw,uploadhw,uploaddpp,uploaddiary,addnotification,fees1,logout1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,14 @@ public class C1A_Home extends AppCompatActivity {
         schoolalbum = findViewById(R.id.schoolalbum);
         contactus = findViewById(R.id.contactus);
         logout = findViewById(R.id.logout);
+        addstudent=findViewById(R.id.addstudent);
+        uploadcw=findViewById(R.id.upload_cw);
+        uploadhw=findViewById(R.id.upload_hw);
+        uploaddpp=findViewById(R.id.upload_dpp);
+        uploaddiary=findViewById(R.id.add_diary);
+        addnotification=findViewById(R.id.add_notification);
+        fees1=findViewById(R.id.fees);
+        logout1=findViewById(R.id.logout_teacher);
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -118,6 +129,60 @@ public class C1A_Home extends AppCompatActivity {
                 finish();
             }
         });
+
+        addstudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(C1A_Home.this,C1A_StudentDetail.class);
+                startActivity(intent);
+            }
+        });
+        uploadcw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cwpdf = new Intent(C1A_Home.this,C1A_Classwork.class);
+                startActivity(cwpdf);
+            }
+        });
+        uploadhw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cwpdf = new Intent(C1A_Home.this,C1A_Homework.class);
+                startActivity(cwpdf);
+            }
+        });
+        uploaddpp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cwpdf = new Intent(C1A_Home.this,C1A_DPP.class);
+                startActivity(cwpdf);
+            }
+        });
+        uploaddiary.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cwpdf = new Intent(C1A_Home.this,C1A_Diary.class);
+                startActivity(cwpdf);
+            }
+        });
+        addnotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cwpdf = new Intent(C1A_Home.this,C1A_Notification.class);
+                startActivity(cwpdf);
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cwpdf = new Intent(C1A_Home.this,LoginPage.class);
+                startActivity(cwpdf);
+                finish();
+            }
+        });
+
+
     }
 
     public static void openDrawer(DrawerLayout drawerLayout){
